4c23fc2884f8f359f4a68528fbec29ec9201c88b
Jesús García Valadez <zerovamp@hotmail.com>
2015-05-15 03:01:44 -0500

    Change error displays

--- assets/snippets/config/config.php
+++ assets/snippets/config/config.php
@@ -1,7 +1,7 @@
 <?php
 //error_reporting(E_ERROR);
-error_reporting( E_ALL | E_STRICT ) ;
-ini_set( 'display_errors', 'On' );
+//error_reporting( E_ALL | E_STRICT ) ;
+ini_set( 'display_errors', 'Off' );
 date_default_timezone_set( 'America/Mexico_City' );
 
 define( 'BASE_PATH', dirname(dirname(dirname((dirname(__FILE__))))) . DIRECTORY_SEPARATOR );
